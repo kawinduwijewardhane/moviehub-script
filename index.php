685a6ddab44b7fabb18572326ade48063885265b
Kawindu Nirmal <66837319+kawindunirmal@users.noreply.github.com>
2022-09-18 02:54:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/mdb.dark.min.css">
-  <link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/style.css">
+  <link rel="stylesheet" href="/assets/css/mdb.dark.min.css">
+  <link rel="stylesheet" href="/assets/css/style.css">
   <link
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
   rel="stylesheet"
@@ -100,6 +100,6 @@
   </div>
 </footer>
 </div>
-<script src="<?php echo $base_url; ?>/assets/js/mdb.min.js"></script>
+<script src="/assets/js/mdb.min.js"></script>
 </body>
 </html>
