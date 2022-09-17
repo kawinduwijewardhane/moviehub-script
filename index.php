1f81fcc6fc98c96bd242c495931edd73f520d575
Kawindu Nirmal <66837319+kawindunirmal@users.noreply.github.com>
2022-09-18 02:51:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
   rel="stylesheet"
 />
-  <title>Document</title>
+  <title>MovieHub</title>
 </head>
 <body>
 <?php include("./inc/navbar.php") ?>
